e34de8279063ba797c53930c862e755077ce3d4f
Ma3tery <568283486@qq.com>
2019-05-22 15:21:34 +0800

    完成与后台数据库交互 读取整个数据表及分页

--- EWS/src/main/java/com.isoft.action/UserAction.java
+++ EWS/src/main/java/com.isoft.action/UserAction.java
@@ -7,11 +7,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Controller
 @RequestMapping("/user")
 public class UserAction {
     @Autowired
     IUserService userService;
+
     @RequestMapping("/login.do")
     @ResponseBody
     public String login(String uname, String upwd) {
@@ -25,11 +30,26 @@ public class UserAction {
     @ResponseBody
     public String register(String uname, String upwd, String email) {
         /*硬编码*/
-        boolean temp = userService.register(uname,upwd,email);
-          if (temp)
+        boolean temp = userService.register(uname, upwd, email);
+        if (temp)
             return "success";
         else
             return "fault";
 
+    }
+
+    @RequestMapping("/findAllUser.do")
+    @ResponseBody
+    public Map<String, Object> findAllUser(int page, int pageSize) {
+        List<Map<String, Object>> list= userService.findAllUser(page,pageSize);
+        System.out.println(list);
+        Map map = new HashMap();
+        map.put("code",0);
+        map.put("msg","用户信息");
+        map.put("count",list.size());//用户表中总记录数
+        map.put("data",list);
+
+
+        return map;//返回JSON格式数据，但是不能转换，因为找不到JSON消息转换器
     }
 }
